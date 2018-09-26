ceec19af6e0c90d48186e92da66f3b1e4b640f5e
Jananjoy Rajkumar <jananjoy.rajkumar@savoirfairelinux.com>
2018-09-26 16:41:07 -0400

    [ADD]Fiche Instrument

--- project_resource_calendar/models/instrument.py
+++ project_resource_calendar/models/instrument.py
@@ -1,27 +1,46 @@
 # Copyright 2018 Savoir-faire Linux
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 from odoo import fields, models
 
 
 class Instrument(models.Model):
-    _inherit = ['resource.resource']
+    """Resource Calendar Instrument"""
     _name = 'resource.calendar.instrument'
+    _inherit = ['resource.resource']
+    _description = __doc__
 
-    room_id = fields.Many2one(
-        'resource.calendar.room',
-        string='Room',
-        )
-    resource_ids = fields.One2many(
-        string='Resources',
-        comodel_name='resource.resource',
-        inverse_name='name',
+    currency_id = fields.Many2one(
+        'res.currency',
+        string='Currency',
+    )
+    tarification = fields.Monetary(
+        'tarification',
+         currency_field='currency_id',
+    )
+    photos = fields.Binary(
+        "Photo",
+        help="Add Instrument Photo",
+    )
+    group_ids = fields.Many2many(
+        'res.groups',
+        string='Group Type',
     )
     sku = fields.Char(
         string='SKU',
     )
-    item_type = fields.Selection([
+    room_id = fields.Many2one(
+        'resource.calendar.room',
+        string='Room',
+    )
+    category_type = fields.Selection([
         ('equipment', 'Equipment'),
+        ('consumables', 'Consumables'),
         ('instruments', 'Instruments'),
         ('services', 'Services'),
-        ('consumables', 'Consumables'),
-        ], string='Type', index=True, copy=False, default='equipment', track_visibility='onchange')
+        ],
+        string='Type',
+        index=True,
+        copy=False,
+        default='equipment',
+    )
--- project_resource_calendar/models/resource.py
+++ project_resource_calendar/models/resource.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Savoir-faire Linux
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 from odoo import fields, models
 
 
@@ -9,5 +10,15 @@ class Resource(models.Model):
     resource_type = fields.Selection([
         ('user', 'Human'),
         ('material', 'Material'),
-        ('room', 'Room')], string='Resource Type',
-        default='user', required=True)
+        ('room', 'Room')
+    ],
+        string='Resource Type',
+        default='user',
+        required=True,
+    )
+    description = fields.Text(
+        string='Description',
+    )
+    is_bookable = fields.Boolean(
+        string='Is Bookable',
+    )
