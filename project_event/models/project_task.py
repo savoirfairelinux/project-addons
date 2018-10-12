8a6e480e118c6e1ef8932e6bd1f286972d7e749e
Luis <eilstdq@gmail.com>
2018-10-12 14:56:23 -0400

    Creation from template condition

--- project_event/models/project_task.py
+++ project_event/models/project_task.py
@@ -123,6 +123,8 @@ class Task(models.Model):
 
     @api.model
     def create(self, vals):
+        if self.is_from_template:
+            vals['message_follower_ids'] = None
         if 'activity_task_type' in vals:
             if vals['activity_task_type'] == 'activity':
                 vals['code'] = self.env['ir.sequence'] \
