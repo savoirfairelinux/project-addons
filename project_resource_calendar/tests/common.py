43861b1d4acd0d8b59bc53886b97862ef862ee1b
neophilic_insect007 <jananjoy@gmail.com>
2018-12-19 14:57:31 -0500

    [ADD] Integration of Unit Testing Test Cases for Calendar Event Model (#262)
    Only tests from report
    
    Update weekly_report.py
    
    resolved comments

--- project_resource_calendar/tests/common.py
+++ project_resource_calendar/tests/common.py
@@ -9,14 +9,26 @@ class TestCalendarEventCommon(common.TransactionCase):
     def setUp(self):
         super(TestCalendarEventCommon, self).setUp()
 
-        # Usefull models
-        self.Calendar = self.env['calendar.event']
+
         self.Rooms = self.env['resource.calendar.room']
         self.Instruments = self.env['resource.calendar.instrument']
+        self.WeeklyReportWizards = self.env['weekly.report.wizard']
 
         self.room_1 = self.Rooms.create({
             'name': 'Test Room 1',
+            'resource_type': 'room',
+            'allow_double_book': True,
         })
+
         self.instrument_1 = self.Instruments.create({
-            'name': 'Test Intrument 1',
+            'name': 'Test Instrument 1',
+            'resource_type': 'material',
+            'room_id': self.room_1.id,
+            'allow_double_book': True,
         })
+    
+        self.weekly_report_wizard_common = self.WeeklyReportWizards.create({
+                'room_id': self.room_1.id,
+                'recurrency': True,
+                'state': 'open',
+            })
