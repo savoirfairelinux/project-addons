859b27b96aec423140c8176cc399cc3236f56c27
Rim BEN DHAOU <bendhaou.rim@gmail.com>
2018-12-20 16:03:36 -0500

    [Fix] Reorganise code (#270)

--- project_event/__manifest__.py
+++ project_event/__manifest__.py
@@ -42,13 +42,9 @@
         'views/res_partner_category_view.xml',
         'views/project_task_view.xml',
         'views/res_partner_sector_view.xml',
-        'templates/assets.xml',
         'wizard/project_event_wizard_view.xml',
         'views/calendar_event_category_view.xml',
         'wizard/reservation_validation_wiz_view.xml',
-    ],
-    'qweb': [
-        'views/calendar_sidebar.xml',
     ],
     'installable': True,
     'application': False,
