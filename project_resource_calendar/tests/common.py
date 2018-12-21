c41f823503a25628a086fee27e70a05b4d04ebc7
Jananjoy Rajkumar <jananjoy.rajkumar@savoirfairelinux.com>
2018-12-21 15:14:39 -0500

    [FIX] Refactory Calendar Event module Test case code

--- project_resource_calendar/tests/common.py
+++ project_resource_calendar/tests/common.py
@@ -9,7 +9,6 @@ class TestCalendarEventCommon(common.TransactionCase):
     def setUp(self):
         super(TestCalendarEventCommon, self).setUp()
 
-
         self.Rooms = self.env['resource.calendar.room']
         self.Instruments = self.env['resource.calendar.instrument']
         self.WeeklyReportWizards = self.env['weekly.report.wizard']
@@ -19,14 +18,12 @@ class TestCalendarEventCommon(common.TransactionCase):
             'resource_type': 'room',
             'allow_double_book': True,
         })
-
         self.instrument_1 = self.Instruments.create({
             'name': 'Test Instrument 1',
             'resource_type': 'material',
             'room_id': self.room_1.id,
             'allow_double_book': True,
         })
-    
         self.weekly_report_wizard_common = self.WeeklyReportWizards.create({
                 'room_id': self.room_1.id,
                 'recurrency': True,
