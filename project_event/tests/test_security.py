9f20bd9121c184074176f5e4031d862a8d5cd12d
Luis <eilstdq@gmail.com>
2019-01-28 16:20:50 -0500

    [ADD] new tests and corrections to rules

--- project_event/tests/test_security.py
+++ project_event/tests/test_security.py
@@ -1,7 +1,8 @@
-# © 2018 Savoir-faire Linux
+# © 2019 Savoir-faire Linux
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL).
 
 from odoo.addons.project_event.tests.common import TestProjectEventCommon
+
 
 class TestSecurity(TestProjectEventCommon):
 
@@ -16,36 +17,35 @@ class TestSecurity(TestProjectEventCommon):
         self.Users = self.env['res.users']
         self.Employees = self.env['hr.employee']
         self.project_user = self.Users.create({
-                'name': 'User',
-                'login': 'base@test.com',
-                'groups_id': [(6, 0,[self.PROJECT_EVENT_USER.id] )]
-            })
+            'name': 'User',
+            'login': 'base@test.com',
+            'groups_id': [(6, 0, [self.PROJECT_EVENT_USER.id])]
+        })
         self.user_editor = self.Users.create({
-                'name': 'Editor',
-                'login': 'editor@test.com',
-                'groups_id': [(6, 0,[self.PROJECT_EVENT_EDITOR.id] )]
-            })
+            'name': 'Editor',
+            'login': 'editor@test.com',
+            'groups_id': [(6, 0, [self.PROJECT_EVENT_EDITOR.id])]
+        })
         self.user_manager = self.Users.create({
-                'name': 'Manager',
-                'login': 'manager@test.com',
-                'groups_id': [(6, 0,[self.PROJECT_EVENT_MANAGER.id] )]
-            })
+            'name': 'Manager',
+            'login': 'manager@test.com',
+            'groups_id': [(6, 0, [self.PROJECT_EVENT_MANAGER.id])]
+        })
         self.employee_base = self.Employees.create({
-                'name': 'User',
-                'work_email': 'base@test.com',
-                'user_id': self.project_user.id
+            'name': 'User',
+            'work_email': 'base@test.com',
+            'user_id': self.project_user.id
         })
         self.employee_editor = self.Employees.create({
-                'name': 'Editor',
-                'work_email': 'editor@test.com',
-                'user_id': self.user_editor.id
+            'name': 'Editor',
+            'work_email': 'editor@test.com',
+            'user_id': self.user_editor.id
         })
         self.employee_manager = self.Employees.create({
-                'name': 'Manager',
-                'work_email': 'manager@test.com',
-                'user_id': self.user_manager.id
+            'name': 'Manager',
+            'work_email': 'manager@test.com',
+            'user_id': self.user_manager.id
         })
-
 
     def has_access_to_menu(self, user_id, menu_name):
         user = self.env['res.users'].browse(user_id)
@@ -62,6 +62,6 @@ class TestSecurity(TestProjectEventCommon):
     def get_group_access_rules(self, group_id):
         group = self.env['res.groups'].browse(group_id)
         return group.model_access
-    
+
     def has_read_permission(self, access_rule):
         return access_rule.perm_read
