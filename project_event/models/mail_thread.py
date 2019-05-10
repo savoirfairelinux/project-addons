0e2bdc6a8d06532f6702700b2d2046ace28c460f
Jananjoy Rajkumar <jananjoy.rajkumar@savoirfairelinux.com>
2019-05-10 11:22:54 -0400

    [ADD] Historization of the elements related to the EVENT, ACTIVITY, TASK: chatter and audit log

--- project_event/models/mail_thread.py
+++ project_event/models/mail_thread.py
@@ -1,6 +1,7 @@
 # © 2018 Savoir-faire Linux
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import re
 from odoo import models, api, _
 
 
@@ -61,4 +62,90 @@ class MailThread(models.AbstractModel):
         if changes and displays:
             tracking_value_ids = display_values_ids + tracking_value_ids
 
+        type_value = self.get_type(tracking_value_ids)
+
+        if type_value == 'Event':
+            order_fields = self.order_event_fields(tracking_value_ids)
+            return changes, order_fields
+
+        if type_value == 'Activity':
+                order_fields = self.order_activity_fields(tracking_value_ids)
+                return changes, order_fields
+
+        if type_value == 'Task':
+            order_fields = self.order_task_fields(tracking_value_ids)
+            return changes, order_fields
+
         return changes, tracking_value_ids
+
+    @staticmethod
+    def get_type(tracking_values):
+        type = None
+        for index in range(len(tracking_values)):
+            list_value = tracking_values.__getitem__(index)
+            if list_value.__getitem__(2).get('field') == 'code':
+                if bool(re.search(
+                        "[E][V][0-9]{6}$",
+                        list_value.__getitem__(2).get('new_value_char')
+                )):
+                    type = 'Event'
+                if bool(re.search(
+                        "[A][C][0-9]{6}$",
+                        list_value.__getitem__(2).get('new_value_char')
+                )):
+                    type = 'Activity'
+                if bool(re.search(
+                        "[T][A][0-9]{6}$",
+                        list_value.__getitem__(2).get('new_value_char')
+                )):
+                    type = 'Task'
+        return type
+
+    @staticmethod
+    def order_event_fields(tracking_values):
+        event_fields_list = [
+            'name', 'code', 'responsible_id',
+            'user_id', 'notes', 'state', 'partner_id'
+        ]
+        event_tracking_values = []
+        for index in range(len(tracking_values)):
+            for k in range(len(tracking_values)):
+                event = tracking_values.__getitem__(k)
+                if event.__getitem__(2).get('field')\
+                        == event_fields_list[index]:
+                    event_tracking_values.append(event)
+        return event_tracking_values
+
+    @staticmethod
+    def order_activity_fields(tracking_values):
+        activity_fields_list = [
+            'name', 'code', 'responsible_id', 'partner_id', 'project_id',
+            'notes', 'category_id', 'date_start', 'date_end', 'task_state',
+            'room_id', 'resource_type', 'manager_id', 'user_id', 'client_type',
+            'sector_id'
+        ]
+        activity_tracking_values = []
+        for index in range(len(tracking_values)):
+            for k in range(len(tracking_values)):
+                activity = tracking_values.__getitem__(k)
+                if activity.__getitem__(2).get('field')\
+                        == activity_fields_list[index]:
+                    activity_tracking_values.append(activity)
+        return activity_tracking_values
+
+    @staticmethod
+    def order_task_fields(tracking_values):
+        task_fields_list = [
+            'name', 'code', 'responsible_id', 'partner_id', 'project_id',
+            'notes', 'category_id', 'date_start', 'date_end', 'department_id',
+            'employee_ids', 'task_state', 'room_id', 'resource_type',
+            'user_id', 'client_type', 'sector_id', 'rel_date_start',
+            'rel_date_end', 'report_done_required'
+        ]
+        task_tracking_values = []
+        for x in range(len(tracking_values)):
+            for k in range(len(tracking_values)):
+                task = tracking_values.__getitem__(k)
+                if task.__getitem__(2).get('field') == task_fields_list[x]:
+                    task_tracking_values.append(task)
+        return task_tracking_values
