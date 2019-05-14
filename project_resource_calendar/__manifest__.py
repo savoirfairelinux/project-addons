5119129d8def279d71acfe451b062b7ca6c7e014
Rim BEN DHAOU <bendhaou.rim@gmail.com>
2019-05-14 12:44:06 -0400

    [ADD] Add check double bookable partners (#395)
    * [ADD] Add check double bookable partners
    
    * Update project_resource_calendar/models/calendar_event.py

--- project_resource_calendar/__manifest__.py
+++ project_resource_calendar/__manifest__.py
@@ -33,6 +33,8 @@
         'views/resource_view.xml',
         'views/room_view.xml',
         'views/instrument_view.xml',
+        'views/res_partner_view.xml',
+        'views/hr_employee_view.xml',
         'templates/assets.xml',
         'report/calendar_event_report.xml',
         'report/calendar_event_print_template.xml',
