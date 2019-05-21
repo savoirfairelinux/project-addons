4340751b1b975420fa88b8ab54d92ad9d85e742b
Luis <luis.garcia@savoirfairelinux.com>
2019-05-21 11:30:44 -0400

    [UPD] project_event/tests/test_project_event_project.py

--- project_event/tests/test_project_event_project.py
+++ project_event/tests/test_project_event_project.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL).
 
 
-from common import TestProjectEventCommon
+from .common import TestProjectEventCommon
 
 
 class TestProjectEvent(TestProjectEventCommon):
