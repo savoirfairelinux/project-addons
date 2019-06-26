4b28f35237f2b6fdd13434d8c7c76cffe08cec12
Larbi Gharib <43585585+lgharib@users.noreply.github.com>
2019-06-26 16:46:37 -0400

    [FIX] Fixed recurrency overlap issue search by id (#491)
    * [FIX] Fixed recurrency overlap issue search by id
    
    * [FIX] Fixed error on update list view activity double booking (#490)
    
    * [FIX] Fixed event report class table (#489)
    
    * [FIX] Date format in validation error message (#492)
    
    * [FIX] Fixed recurrency overlap issue search by id

--- project_resource_calendar/models/calendar_event.py
+++ project_resource_calendar/models/calendar_event.py
@@ -383,6 +383,13 @@ class CalendarEvent(models.Model):
                 )
         return super(CalendarEvent, self).unlink()
 
+    def remove_recurrent_calendar_event(self, overlaps):
+        overlaps_counter = len(overlaps.ids)
+        for overlap in overlaps:
+            if overlap.id == self.id:
+                overlaps_counter -= 1
+        return overlaps_counter
+
     def is_hr_resource_double_booked(self, attendee,
                                      date_start=None, date_end=None):
         if not date_end and not date_start:
@@ -390,13 +397,13 @@ class CalendarEvent(models.Model):
             date_end = self.date_end
 
         overlaps_partners = self.env['calendar.event'].search([
-            ('id', '!=', self.id),
             ('partner_ids', 'in', attendee),
             ('start', '<', date_end),
             ('stop', '>', date_start),
             ('state', '!=', 'cancelled'),
         ])
-        return len(overlaps_partners) > 0
+
+        return self.remove_recurrent_calendar_event(overlaps_partners) > 0
 
     def get_double_booked_resources(self, date_start=None, date_end=None):
         booked_resources = []
@@ -410,21 +417,20 @@ class CalendarEvent(models.Model):
             ('stop', '>', date_start),
             ('state', '!=', 'cancelled')]
 
-        if self.id:
-            overlap_domain.append(('id', '!=', self.id))
-
         if self.room_id:
             overlap_domain.append(('room_id', '=', self.room_id.id))
             overlaps = self.env['calendar.event'].search(overlap_domain)
             overlap_domain.remove(('room_id', '=', self.room_id.id))
-            if len(overlaps.ids) > 0:
+
+            if self.remove_recurrent_calendar_event(overlaps) > 0:
                 booked_resources.append(self.room_id.name)
 
         for equipment in self.equipment_ids:
             overlap_domain.append(('equipment_ids', 'in', equipment.id))
             overlaps_equipment = self.env['calendar.event']\
                 .search(overlap_domain)
-            if len(overlaps_equipment) > 0:
+
+            if self.remove_recurrent_calendar_event(overlaps_equipment) > 0:
                 booked_resources.append(equipment.name)
 
         for partner in self.partner_ids:
@@ -448,14 +454,12 @@ class CalendarEvent(models.Model):
             ('stop', '>', date_start),
             ('state', '!=', 'cancelled')]
 
-        if self.id:
-            overlap_domain.append(('id', '!=', self.id))
-
         if room_id:
             overlap_domain.append(('room_id', '=', room_id))
             overlaps = self.env['calendar.event'].search(overlap_domain)
             overlap_domain.remove(('room_id', '=', room_id))
-            if len(overlaps.ids) > 0:
+
+            if self.remove_recurrent_calendar_event(overlaps) > 0:
                 booked_resources.append(self.env['resource.calendar.room']
                                         .browse(room_id).name)
 
@@ -463,7 +467,8 @@ class CalendarEvent(models.Model):
             overlap_domain.append(('equipment_ids', 'in', equipment))
             overlaps_equipment = self.env['calendar.event']\
                 .search(overlap_domain)
-            if len(overlaps_equipment) > 0:
+
+            if self.remove_recurrent_calendar_event(overlaps_equipment) > 0:
                 booked_resources.append(self
                                         .env['resource.calendar.instrument']
                                         .browse(equipment).name)
