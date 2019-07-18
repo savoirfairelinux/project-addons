03e66a21bc540f0acd8318d95ea9f9cd14bb0c8e
Luis <luis.garcia@savoirfairelinux.com>
2019-07-18 11:41:37 -0400

    Update project_event/static/src/js/disable_archive_action.js
    Co-Authored-By: Rim BEN DHAOU <rim.bendhaou@savoirfairelinux.com>

--- project_event/static/src/js/disable_archive_action.js
+++ project_event/static/src/js/disable_archive_action.js
@@ -9,7 +9,7 @@ odoo.define('test_modulo.BasicView', function (require) {
                 this._super.apply(this, arguments);
                 var model = self.controllerParams.modelName in ['res.partner','project.task'] ? 'True' : 'False';
                 if(model) {
-                    session.user_has_group('project_event.group_project_event_manage').then(function(has_group) {
+                    session.user_has_group('project_event.group_project_event_manager').then(function(has_group) {
                         if(!has_group) {
                             self.controllerParams.archiveEnabled = 'False' in viewInfo.fields;
                         }
