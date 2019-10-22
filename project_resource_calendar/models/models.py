ff69f2f6ac93c54391e98d9c78e9fa69cb2ba43f
eilst <luis.garcia@savoirfairelinux.com>
2019-10-22 12:27:01 -0400

    [ADD] Export capability of virtual events

--- project_resource_calendar/models/models.py
+++ project_resource_calendar/models/models.py
@@ -38,7 +38,13 @@ def _export_rows(self, fields, batch_invalidate=True, virtual_data=None):
             return rs
     # both _ensure_xml_id and the splitter want to work on recordsets but
     # neither returns one, so can't really be composed...
-    xids = dict(self.__ensure_xml_id(skip=['id'] not in fields))
+    uniq_ids = self.browse(list(dict.fromkeys(self.ids)))
+    if virtual_data and False not in virtual_data:
+        xids = dict(
+            uniq_ids.__ensure_xml_id(
+                skip=['id'] not in fields))
+    else:
+        xids = dict(self.__ensure_xml_id(skip=['id'] not in fields))
     # memory stable but ends up prefetching 275 fields (???)
     for record in splittor(self):
         # main line of record, initially empty
@@ -49,6 +55,8 @@ def _export_rows(self, fields, batch_invalidate=True, virtual_data=None):
         primary_done = []
 
         # process column by column
+        virtual_data_current = virtual_data.pop(
+            0) if virtual_data else None
         for i, path in enumerate(fields):
             if not path:
                 continue
@@ -66,13 +74,13 @@ def _export_rows(self, fields, batch_invalidate=True, virtual_data=None):
             else:
                 field = record._fields[name]
                 if isinstance(field, odoo.fields.Datetime):
-                    virtual_data_current = virtual_data.pop(
-                        0) if virtual_data else None
                     if virtual_data_current:
                         if name == 'start_datetime' or name == 'start':
                             value = virtual_data_current[0]
                         if name == 'stop_datetime' or name == 'stop':
                             value = virtual_data_current[1]
+                        lines[-1][0] = '__export__.' + (record._name).replace(
+                            '.', '_') + '_' + str(record.id) + '_recurrent_virtual'
                     else:
                         try:
                             value = \
@@ -140,7 +148,6 @@ def __ensure_xml_id(self, skip=False):
             % (self._name, self._table))
 
     modname = '__export__'
-
     cr = self.env.cr
     cr.execute("""
         SELECT res_id, module, name
