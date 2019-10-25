5fb44ba381f37aca6140188f616cd799b499b4fc
Luis <luis.garcia@savoirfairelinux.com>
2019-10-25 15:47:34 -0400

    [FIX] write timesheets, create (#575)
    * [FIX] write timesheets, create
    
    * [FIX] domain of department and function

--- project_task_shift_timesheet/models/project_task_shift_timesheet.py
+++ project_task_shift_timesheet/models/project_task_shift_timesheet.py
@@ -18,11 +18,11 @@ class TaskShiftTimesheet(models.Model):
         domain="[('activity_task_type', 'in', ['activity'])]",
     )
     activity_code = fields.Char(
-        related='activity_id.code',
         string='Number'
     )
     activity_date = fields.Datetime(
-        related='activity_id.date_start',
+        compute='_compute_activity_date',
+        store=True
     )
     department = fields.Many2one(
         'hr.department',
@@ -37,7 +37,7 @@ class TaskShiftTimesheet(models.Model):
         string='Employee',
         track_visibility='onchange',
         default=lambda self: self.env['hr.employee'].search([
-            ('user_id', '=', self.env.user.id)])
+            ('user_id', '=', self.env.user.id)])[0]
     )
     shift = fields.Integer(default=1)
     start_hour = fields.Integer(
@@ -59,7 +59,12 @@ class TaskShiftTimesheet(models.Model):
     shift_status = fields.Boolean(
         default=False
     )
-
+    compute_dep = fields.Char(
+        default=lambda self: self._compute_dep(),
+        store=False)
+    compute_function = fields.Char(
+        default=lambda self: self._compute_function(),
+        sore=False)
     _sql_constraints = [
         ('check_shift_start_and_end_time',
          "CHECK ((start_hour < end_hour)) ",
@@ -82,7 +87,7 @@ class TaskShiftTimesheet(models.Model):
                                            self.end_minute, 0) -
                          datetime.datetime(2019, 1, 1, self.start_hour,
                                            self.start_minute, 0))\
-            .total_seconds()/60/60
+            .total_seconds() / 60 / 60
 
     @api.one
     def _compute_default_name(self):
@@ -90,3 +95,43 @@ class TaskShiftTimesheet(models.Model):
 
     def approve_shift(self):
         self.shift_status = True
+
+    @api.depends('activity_id')
+    @api.one
+    def _compute_activity_date(self):
+        if self.activity_id:
+            self.activity_date = self.activity_id[0].date_start
+
+    @api.model
+    def create(self, vals):
+        if 'activity_id' in vals:
+            vals['activity_code'] = self.env['project.task'].browse(
+                vals['activity_id']).code
+            vals['activity_date'] = self.env['project.task'].browse(
+                vals['activity_id']).date_start
+        return super(TaskShiftTimesheet, self).create(vals)
+
+    def _compute_dep(self):
+        departments = []
+        if 'activity_id' in self._context:
+            activity = self.env['project.task'].browse(
+                self._context['activity_id'])
+            current_employee = self.env['res.users'].browse(
+                self._context['uid']).employee_ids[0]
+            for child in activity.child_ids:
+                if current_employee in child.employee_ids:
+                    departments.append(child.department_id.id)
+        return departments
+
+    @api.one
+    def _compute_function(self):
+        services = []
+        if 'activity_id' in self._context:
+            activity = self.env['project.task'].browse(
+                self._context['activity_id'])
+            current_employee = self.env['res.users'].browse(
+                self._context['uid']).employee_ids[0]
+            for child in activity.child_ids:
+                if current_employee in child.employee_ids:
+                    services.append(child.service_id.id)
+        return services
