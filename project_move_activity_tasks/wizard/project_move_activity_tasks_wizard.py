221c10c1b6d3e5275d867a43b023b0fb896e5618
Luis <luis.garcia@savoirfairelinux.com>
2020-01-07 17:34:13 -0500

    [UPD] LGPL-3.0 -> AGPL-3.0 (#625)

--- project_move_activity_tasks/wizard/project_move_activity_tasks_wizard.py
+++ project_move_activity_tasks/wizard/project_move_activity_tasks_wizard.py
@@ -1,5 +1,5 @@
 # @ 2018 Savoir-failre Linux
-# License LGPL-3.0 or Later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 from odoo import _, api, fields, models
