600918068d7ddde80e319ac48634ff47265aa411
Luis <luis.garcia@savoirfairelinux.com>
2020-04-14 19:00:31 -0400

    [FIX] can create time shift less than an hour (#635)
    * [FIX] can create time shift less than an hour
    
    * [FIX] tests, removed not significant audit log tests

--- project_resource_calendar/tests/__init__.py
+++ project_resource_calendar/tests/__init__.py
@@ -2,7 +2,5 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/LGPL).
 
 from . import test_weekly_report_wizard
-from . import test_instrument
-from . import test_room
 from . import test_calendar_event
 from . import test_security
