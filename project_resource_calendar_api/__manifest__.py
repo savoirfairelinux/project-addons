9b35c78f2a9e7ca1d42a78d826ae04745692cd11
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2020-04-15 17:38:11 -0400

    [ADD] Added api to project resource calendar

---
+++ project_resource_calendar_api/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Savoir-faire Linux
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project resource calendar api",
+    "summary": "API to expose project resource calendar data in JSON format",
+    "version": "11.0.1.0.0",
+    "category": "Web",
+    "website": "https://github.com/savoirfairelinux/project-addons",
+    'author': 'Savoir-faire Linux, Odoo Community Association (OCA)',
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project_event",
+    ]
+}
